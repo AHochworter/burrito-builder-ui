978335d964a571c966dc3390de59ec9b9f360820
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-13 09:32:58 -0700

    Identify initial bugs for app to load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,39 @@
-import { useEffect } from "react";
-import "./App.css";
-import { getOrders } from "../../apiCalls";
-import Orders from "../../components/Orders/Orders";
-import OrderForm from "../../components/OrderForm/OrderForm";
+import { useState, useEffect } from 'react';
+import './App.css';
+import { getOrders } from '../../apiCalls';
+import Orders from '../../components/Orders/Orders';
+import OrderForm from '../../components/OrderForm/OrderForm';
 
 function App() {
+  const dummyOrders = [
+    {
+      id: 1,
+      name: 'Pat',
+      ingredients: ['beans', 'lettuce', 'carnitas', 'queso fresco', 'jalapeno'],
+    },
+    {
+      id: 2,
+      name: 'Johann',
+      ingredients: [
+        'sofritas',
+        'lettuce',
+        'carnitas',
+        'queso fresco',
+        'jalapeno',
+      ],
+    },
+    {
+      id: 3,
+      name: 'Nicole',
+      ingredients: ['beans', 'lettuce', 'carnitas', 'queso fresco', 'jalapeno'],
+    },
+  ];
+
   useEffect(() => {
-    getOrders().catch((err) => console.error("Error fetching:", err));
+    getOrders().catch(err => console.error('Error fetching:', err));
   });
+
+  const [orders, setOrders] = useState(dummyOrders);
 
   return (
     <main className="App">
@@ -16,7 +42,7 @@ function App() {
         <OrderForm />
       </header>
 
-      <Orders orders={"Here is where orders go"} />
+      <Orders orders={orders} />
     </main>
   );
 }
--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function OrderForm(props) {
-  const [name, setName] = useState("");
+function OrderForm({ orders }) {
+  const [name, setName] = useState('');
   const [ingredients, setIngredients] = useState([]);
+
+  function handleIngredientClick(ingredient) {
+    if (ingredients.includes(ingredient)) {
+      // If ingredient already exists, remove it
+      const newIngredients = ingredients.filter(item => item !== ingredient);
+      setIngredients(newIngredients);
+    } else {
+      // else, add ingredient
+      setIngredients([...ingredients, ingredient]);
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -10,30 +21,30 @@ function OrderForm(props) {
   }
 
   function clearInputs() {
-    setName("");
+    setName('');
     setIngredients([]);
-  };
+  }
 
   const possibleIngredients = [
-    "beans",
-    "steak",
-    "carnitas",
-    "sofritas",
-    "lettuce",
-    "queso fresco",
-    "pico de gallo",
-    "hot sauce",
-    "guacamole",
-    "jalapenos",
-    "cilantro",
-    "sour cream",
+    'beans',
+    'steak',
+    'carnitas',
+    'sofritas',
+    'lettuce',
+    'queso fresco',
+    'pico de gallo',
+    'hot sauce',
+    'guacamole',
+    'jalapenos',
+    'cilantro',
+    'sour cream',
   ];
-  const ingredientButtons = possibleIngredients.map((ingredient) => {
+  const ingredientButtons = possibleIngredients.map(ingredient => {
     return (
       <button
         key={ingredient}
         name={ingredient}
-        // onClick={(e) => }
+        onClick={() => handleIngredientClick(ingredient)}
       >
         {ingredient}
       </button>
@@ -47,14 +58,14 @@ function OrderForm(props) {
         placeholder="Name"
         name="name"
         value={name}
-        // onChange={(e) => }
+        onChange={event => setName(event.target.value)}
       />
 
       {ingredientButtons}
 
-      <p>Order: {ingredients.join(", ") || "Nothing selected"}</p>
+      <p>Order: {ingredients.join(', ') || 'Nothing selected'}</p>
 
-      <button onClick={(e) => handleSubmit(e)}>Submit Order</button>
+      <button onClick={e => handleSubmit(e)}>Submit Order</button>
     </form>
   );
 }
